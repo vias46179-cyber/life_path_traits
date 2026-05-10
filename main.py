362ffa8d85c41de18224d7473b52281e0c7b2df6
vias46179-cyber <vias46179@gmail.com>
2026-05-10 16:50:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+b= input("May i ask your date of birth\n(just type it without slashes)\n(Also type the complete year)")
+z= input("can i ask your name ?")
+print("GREAT " +  "  ********"  + z + "*******")
+c = int(b[0])
+d = int(b[1])
+c = int(b[0])
+d= int(b[1])
+e= int(b[2])
+f= int(b[3])
+g= int(b[4])
+h = int(b[5])
+i = int(b[6])
+j = int(b[7])
+k = c + d + e + f + g + h + i + j
+l = str(k)
+m = int(l[0])+ int(l[1])
+print("your life path's magic number is " + str(m))
+print("Your life will be with these traits...")
+if m == 1:
+    print("Leader; independent, ambitious, pioneering, self-motivated.")
+if m == 2:
+    print("Diplomat; cooperative, sensitive, intuitive, harmony-seeking.")
+if m == 3:
+    print("Creative communicator; expressive, social, optimistic, artistic.")
+if m == 4:
+    print("Builder; practical, disciplined, reliable, structured.")
+if m == 5:
+    print("Adventurer; freedom-loving, adaptable, energetic, curious.")
+if m == 6:
+    print( "Nurturer; responsible, caring, protective, family-oriented.")
+if m == 7:
+    print( "Thinker; analytical, introspective, spiritual, truth-seeking.")
+if m == 8:
+    print("Achiever; ambitious, powerful, goal-driven, success-oriented.")
+if m == 9:
+    print( "Humanitarian; compassionate, idealistic, generous, service-minded.")
+if m == 11:
+    print( "intuitive, visionary, inspiring, emotionally sensitive.")
+if m == 22 :
+    print("master builder, practical visionary, leadership through action.")
+if m == 33 :
+    print("compassionate teacher, spiritual, healing, selfless.")
+
